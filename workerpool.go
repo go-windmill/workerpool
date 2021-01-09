ad8427d2436cb5ab7106f5d9d8aa6c8ca8647cad
Charles Marshall <cm56marshall@googlemail.com>
2021-01-09 10:02:21 +0000

    worker pool with testing in place and some state / meta details

--- workerpool.go
+++ workerpool.go
@@ -14,28 +14,45 @@ type Task struct {
 	Namspace string
 }
 
+// Workers is
+type Workers struct {
+	Started int
+	Stopped int
+}
+
+// Tasks is
+type Tasks struct {
+	Counter int
+}
+
+// State is
+type State struct {
+	Workers Workers
+	Tasks   Tasks
+}
+
 // WorkerPool is
 type WorkerPool struct {
-	Namespace  string
-	MaxWorkers int
-	Exit       chan bool
-
+	Namespace string
+	State     State
 	WaitGroup *sync.WaitGroup
 	Mutex     *sync.Mutex
-
-	Tasks chan Task
+	Exit      chan bool
+	Queue     chan Task
 }
 
 // Start is
 func (w *WorkerPool) Start(count int) {
+	fmt.Println("Starting workers", count)
 	for i := 0; i < count; i++ {
 		w.WaitGroup.Add(1)
-		fmt.Println("New worker", i)
+		w.State.Workers.Started++
 		go worker(
 			w.Namespace,
 			i,
-			w.Tasks,
+			w.Queue,
 			w.Exit,
+			&w.State,
 			w.WaitGroup,
 			w.Mutex)
 	}
@@ -43,40 +60,48 @@ func (w *WorkerPool) Start(count int) {
 
 // Stop is
 func (w *WorkerPool) Stop(count int) {
+	fmt.Println("Stopping workers", count)
+
+	// exit i number of items
 	for i := 0; i < count; i++ {
-		<-w.Exit
+		w.Exit <- true
 	}
+}
+
+// Running is
+func (w *WorkerPool) Running() int {
+	return w.State.Workers.Started - w.State.Workers.Stopped
 }
 
 // Add is
 func (w *WorkerPool) Add(task func()) {
-
-	if task != nil {
+	// task is real and there is a worker running
+	if task != nil && w.State.Workers.Started > 0 {
 		t := Task{ID: uuid.New(), F: task, Namspace: w.Namespace}
 		fmt.Println("Adding a task", t)
-		w.Tasks <- t
+		w.Queue <- t
 	}
 }
 
 // CloseAndWait is
 func (w *WorkerPool) CloseAndWait() {
-	close(w.Tasks)
+	close(w.Queue)
 	w.WaitGroup.Wait()
 }
 
 // New is
-func New(namespace string, count int) *WorkerPool {
+func New(namespace string) *WorkerPool {
 	var wg sync.WaitGroup
 
 	wp := WorkerPool{
-		Namespace:  namespace,
-		MaxWorkers: count,
-		Exit:       make(chan bool),
-		WaitGroup:  &wg,
-		Mutex:      &sync.Mutex{},
-		Tasks:      make(chan Task, 1)}
-
-	wp.Start(count)
+		Namespace: namespace,
+		WaitGroup: &wg,
+		Mutex:     &sync.Mutex{},
+		Exit:      make(chan bool),
+		Queue:     make(chan Task, 1),
+		State: State{
+			Workers{Started: 0, Stopped: 0},
+			Tasks{Counter: 0}}}
 
 	return &wp
 }
@@ -86,6 +111,7 @@ func worker(
 	index int,
 	tasks <-chan Task,
 	quit <-chan bool,
+	state *State,
 	wg *sync.WaitGroup,
 	mu *sync.Mutex) {
 
@@ -95,13 +121,16 @@ func worker(
 		select {
 		case task, ok := <-tasks:
 			if !ok {
-				fmt.Printf("[%s:%d] worker failed to retrieve task: \n%v\n", namespace, index, ok)
+				fmt.Printf("[%s:%d] no task, quiting\n", namespace, index)
+				state.Workers.Stopped++
 				return
 			}
 			fmt.Printf("[%s:%d][%s:%s] worker has recieved task\n", namespace, index, task.Namspace, task.ID)
 			task.F()
+			state.Tasks.Counter++
 		case <-quit:
 			fmt.Printf("[%s:%d] worker has quit\n", namespace, index)
+			state.Workers.Stopped++
 			return
 
 		}
--- workerpool_test.go
+++ workerpool_test.go
@@ -1,18 +1,148 @@
 package workerpool
 
 import (
-	"fmt"
+	"log"
+	"math/rand"
 	"testing"
 )
 
+func testFuncWithPointer(i *int) {
+	*i++
+}
+
 func Test_New_Returned(t *testing.T) {
 	t.Parallel()
+	// Create a new worker pool with name of testNew
+	p := New("testNew")
+	// close it done and wait
+	p.CloseAndWait()
+	// as we didnt start any workers counters should all
+	// be 0
+	if p.State.Workers.Started > 0 ||
+		p.State.Workers.Stopped > 0 ||
+		p.State.Tasks.Counter > 0 {
 
-	p := New("test", 2)
+		log.Fatalln("No pool started or tasks added so counters should be 0, but weren't.", p.State)
+	}
+}
 
-	p.Add(func() {
-		fmt.Println("Handling...")
-	})
+func Test_Start(t *testing.T) {
+	t.Parallel()
+	// create a worker pool
+	p := New("testStart")
+	size := rand.Intn(10-5) + 5
+	// generate a least 5 workers
+	p.Start(size)
+	// close them all down
+	p.CloseAndWait()
+	// check counters
+	if p.State.Workers.Started != size ||
+		p.State.Workers.Stopped != size {
+		log.Fatalf("Created a pool of (%v) workers but counters after closing did not match.\n%v\n", size, p.State.Workers)
+	}
+	if p.State.Tasks.Counter > 0 {
+		log.Fatalf("Create a pool of (%v) workers without adding a task to the queue, so counter should be 0\n%v\n", size, p.State.Tasks)
+	}
+}
+
+func Test_Multiple_Starts(t *testing.T) {
+	t.Parallel()
+
+	p := New("testMultiStarts")
+	p.Start(5)
+	p.Start(1)
+	p.Start(1)
 
 	p.CloseAndWait()
+
+	if p.State.Workers.Started != 7 {
+		log.Fatalf("Total number of workers started does not matched the expected value, expected %v, got %v\n", 7, p.State.Workers.Started)
+	}
+}
+
+func Test_Multiple_StartStops(t *testing.T) {
+	t.Parallel()
+
+	p := New("testMultiStarts")
+	p.Start(5)
+	r1 := p.Running()
+	p.Stop(1)
+	r2 := p.Running()
+	p.Stop(1)
+	r3 := p.Running()
+	p.Start(1)
+
+	p.CloseAndWait()
+
+	if r1 != 5 {
+		log.Fatalf("Initial running count does not match expected pool size. Expected %v, got %v\n", 5, r1)
+	}
+	if r2 != 4 {
+		log.Fatalf("Modified running count does not match expected pool size. Expected %v, got %v\n", 4, r2)
+	}
+	if r3 != 3 {
+		log.Fatalf("Modified (again) running count does not match expected pool size. Expected %v, got %v\n", 3, r3)
+	}
+	if p.State.Workers.Started != 6 {
+		log.Fatalf("End worker pool count does not match. Expected %v, got %v\n", 6, p.State.Workers.Started)
+	}
+}
+
+func Test_Add_Simple(t *testing.T) {
+	t.Parallel()
+	p := New("testSimpleAdd")
+
+	x := 0
+	workers := 2
+	tasks := 5
+
+	p.Start(workers)
+	for i := 0; i < tasks; i++ {
+		p.Add(func() {
+			testFuncWithPointer(&x)
+		})
+	}
+	p.CloseAndWait()
+
+	if x != tasks {
+		log.Fatalf("Error running tasks, counter value incorrect. Expected %v, got %v\n", tasks, x)
+	}
+	if p.State.Workers.Started != workers {
+		log.Fatalf("Error with worker pool size. Expected %v, got %v\n", workers, p.State.Workers.Started)
+	}
+
+}
+
+func Test_Add_With_ExpandingPool(t *testing.T) {
+	t.Parallel()
+	p := New("textExpanding")
+
+	x := 0
+	workers := 2
+	tasks := 50
+	half := tasks / 2
+	p.Start(workers)
+
+	for i := 0; i < tasks; i++ {
+		p.Add(func() {
+			testFuncWithPointer(&x)
+		})
+		// should run twice
+		if i%half == 0 {
+			p.Start(1)
+		}
+	}
+
+	p.CloseAndWait()
+
+	if x != tasks {
+		log.Fatalf("Error running tasks, counter value incorrect. Expected %v, got %v\n", tasks, x)
+	}
+	if p.State.Workers.Started != 4 {
+		log.Fatalf("Pool has not expanded to the expected size. Expected %v, got %v\n", 4, p.State.Workers.Started)
+	}
+	if p.State.Tasks.Counter != 50 {
+		log.Fatalf("Task counter does not match. Expected %v, got %v\n", tasks, p.State.Tasks.Counter)
+	}
+
 }
